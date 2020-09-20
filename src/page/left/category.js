841900698c068b4ee9730bbc117fe4ab286c969c
Aiden <aidengookjinkim@gmail.com>
2020-09-19 21:01:48 -0600

    Create category

--- src/page/left/category.js
+++ src/page/left/category.js
@@ -9,6 +9,7 @@ class category extends Component {
     super(props)
     this.state = {
       category: [],
+      edit: false,
     }
   }
 
@@ -22,10 +23,29 @@ class category extends Component {
     this.setState({category: getData.data});
   } 
 
+  _addCategory = async () => {
+    let category_name = window.prompt('Please enter a name for the category you want to add.');
+    category_name = category_name.trim();
+
+    if(category_name !== '' && category_name.length > 0) {
+      const add = await axios('/add/category', {
+        method: 'POST',
+        data: {name: category_name},
+        headers: new Headers()
+      })
+
+      alert(add.data.msg);
+      this._getCategoryData();
+      
+    } else {
+      return alert('You must enter at least 1 letter.')
+    }
+  }
+
   render() {
-    const {category} = this.state;
-    const {_changeCategory} = this.props;
-    let pre_cat = '';
+    const {category, edit} = this.state;
+    const {_changeCategory, login} = this.props;
+        let pre_cat = '';
 
     if(sessionStorage.getItem('category')) {
       pre_cat = Number(sessionStorage.getItem('category'));
@@ -34,12 +54,24 @@ class category extends Component {
     return (
       <div className='Category'>
         <ul>
-          <li><Link className={pre_cat === '' ? 'pre_cat' : null} to='/' onClick={() =>_changeCategory('')}>View All</Link> <hr /></li>
+          <li><Link className={pre_cat === '' ? 'pre_cat' : null} to='/' onClick={() =>_changeCategory('')}>View All</Link>
+          {login ? !edit ? <input type='button' value='Edit' className='Edit' onClick={() => this.setState({edit: !edit})}/> : 
+                            <input type='button' value='Add' className='Edit' onClick={() => this._addCategory()}/> : null}<hr/></li>
           {category.length > 0 ? 
             category.map( (el, key) => {
-              return(
-              <li key={key}><Link className={pre_cat === el.id ? 'pre_cat' : null} to='/' onClick={() => _changeCategory(el.id)}>{el.name}</Link></li>
-              )
+              if(!edit) {
+                return(
+                  <li key={key}><Link className={pre_cat === el.id ? 'pre_cat' : null} to='/' onClick={() => _changeCategory(el.id)}>{el.name}</Link></li>
+                  )
+              } else {
+                return(
+                  <li key={key}>
+                    <img src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjQgMy43NTJsLTQuNDIzLTMuNzUyLTcuNzcxIDkuMDM5LTcuNjQ3LTkuMDA4LTQuMTU5IDQuMjc4YzIuMjg1IDIuODg1IDUuMjg0IDUuOTAzIDguMzYyIDguNzA4bC04LjE2NSA5LjQ0NyAxLjM0MyAxLjQ4N2MxLjk3OC0xLjMzNSA1Ljk4MS00LjM3MyAxMC4yMDUtNy45NTggNC4zMDQgMy42NyA4LjMwNiA2LjY2MyAxMC4yMjkgOC4wMDZsMS40NDktMS4yNzgtOC4yNTQtOS43MjRjMy4yODctMi45NzMgNi41ODQtNi4zNTQgOC44MzEtOS4yNDV6Ii8+PC9zdmc+' className='remove_icon'/>
+                    <input type='text' maxLength='20' className='edit_input' defaultValue={el.name}/>
+                  </li>
+                )
+              }
+              
             })
         : null }
         </ul>
