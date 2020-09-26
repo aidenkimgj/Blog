b5172a4b7f33791a400675c106d627a43a0eff68
Aiden <aidengookjinkim@gmail.com>
2020-09-26 14:24:06 -0600

    change table

--- server/model.js
+++ server/model.js
@@ -17,8 +17,8 @@ module.exports = {
   
   api: {
     searchInfo: (body, hash, callback) => {
-      Admin.findAll({
-        where: {[Op.and]: [{user_id: body.id, password: hash}]}
+      User.findAll({
+        where: {[Op.and]: [{id: body.id, password: hash}]}
       })
       .then(data => {
         callback(data);
