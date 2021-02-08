92dc1be87aa5abda18e17487e40e610019e03c8a
Aiden Kim <aidengookjinkim@gmail.com>
2021-02-07 18:58:41 -0700

    update

--- server/controller.js
+++ server/controller.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
 const path = require('path');
 const model = require('./model');
 const salt = require(path.join(__dirname, 'config', 'db.json')).salt;
 const hashing = require(path.join(__dirname, 'config', 'hashing.js'));
 const moment = require('moment-timezone');
-moment.tz.setDefault("America/Calgary");
+moment.tz.setDefault('America/Calgary');
 const now_date = moment().format('YYYY-MM-DD HH:mm:ss');
 const user_ip = require('ip');
 const personal_info = require(path.join(__dirname, 'config', 'personal.js'));
@@ -14,7 +15,7 @@ const mailPoster = nodeMailer.createTransport({
   auth: {
     user: 'gksxogns2000@gmail.com',
     pass: personal_info.email_pass(),
-  }
+  },
 });
 
 const mailOpt = (user_data, title, contents) => {
@@ -24,55 +25,55 @@ const mailOpt = (user_data, title, contents) => {
     subject: title,
     text: contents,
   };
-  
+
   return mailOption;
-}
+};
 
 const send_Mail = mailOption => {
   mailPoster.sendMail(mailOption, (error, info) => {
-    if(error) {
+    if (error) {
       console.log('Error', error);
     } else {
       console.log('Send has been completed', info.response);
     }
   });
-}
-console.log("컨트롤러 불림");
+};
+console.log('컨트롤러 불림');
 module.exports = {
   needs: () => upload,
   api: {
-      sendPw: (req, res) => {
-        const body = req.body;
-        const hash = hashing.enc(body.id, body.password, salt);
+    sendPw: (req, res) => {
+      const body = req.body;
+      const hash = hashing.enc(body.id, body.password, salt);
 
-        model.api.searchInfo(body, hash, result => {
-          let obj = {};
-          
-          if(result[0]) {
-            obj['suc'] = result[0].dataValues;
-            obj['msg'] = 'Login Sucessful';
-            obj['ip'] = user_ip.address();
-          } else {
-            obj['suc'] = false;
-            obj['msg'] = 'Login Failed';
-          }
-          res.send(obj); 
-        });
-      },
+      model.api.searchInfo(body, hash, result => {
+        let obj = {};
 
-      getIp: (req, res) => {
-        const ip = personal_info.ip();
-        res.send(ip);
-      }
+        if (result[0]) {
+          obj['suc'] = result[0].dataValues;
+          obj['msg'] = 'Login Sucessful';
+          obj['ip'] = user_ip.address();
+        } else {
+          obj['suc'] = false;
+          obj['msg'] = 'Login Failed';
+        }
+        res.send(obj);
+      });
+    },
+
+    getIp: (req, res) => {
+      const ip = personal_info.ip();
+      res.send(ip);
+    },
   },
-  
+
   add: {
     board: (req, res) => {
       // console.log(req.body);
       const body = req.body;
 
       model.add.board(body, result => {
-        if(result) {
+        if (result) {
           res.send(true);
         }
       });
@@ -83,9 +84,9 @@ module.exports = {
 
       model.add.category(body, result => {
         let obj = {};
-        if(result) {
+        if (result) {
           obj['suc'] = true;
-          obj['msg'] = 'Category has been created.'; 
+          obj['msg'] = 'Category has been created.';
         } else {
           obj['suc'] = false;
           obj['msg'] = 'This category already exists.';
@@ -99,9 +100,8 @@ module.exports = {
       const hash_pw = hashing.enc(body.id, body.password, salt);
 
       model.add.user(body, hash_pw, now_date, result => {
-        
         res.send(result);
-      })
+      });
     },
 
     reply: (req, res) => {
@@ -109,8 +109,8 @@ module.exports = {
 
       model.add.reply(body, now_date, result => {
         res.send(result);
-      })
-    }
+      });
+    },
   },
 
   get: {
@@ -118,7 +118,7 @@ module.exports = {
       const body = req.body;
       console.log(body);
       model.get.board(body, result => {
-        if(result) {
+        if (result) {
           res.send(result);
         }
       });
@@ -128,7 +128,7 @@ module.exports = {
       const body = req.body;
 
       model.get.board_cnt(body, cnt => {
-        const result = {cnt: cnt}
+        const result = { cnt: cnt };
         res.send(result);
       });
     },
@@ -137,13 +137,12 @@ module.exports = {
       const body = req.body;
 
       model.get.board_data(body, data => {
-        const result = {data: data}
+        const result = { data: data };
         res.send(result);
       });
     },
 
     category: (req, res) => {
-
       model.get.category(data => {
         res.send(data);
       });
@@ -162,34 +161,32 @@ module.exports = {
 
       model.get.reply_data(body, data => {
         res.send(data);
-      })
-    }
+      });
+    },
   },
 
   update: {
     view_cnt: (req, res) => {
       const body = req.body;
-            
+
       const expires = new Date();
-      
+
       expires.setDate(expires.getDate() + 1);
-      
+
       const cookie_name = `board_${body.id}_user_${body.user_id}`;
       const exist_cookie = req.cookies[cookie_name];
-      
 
-    
-      if(!exist_cookie) {
+      if (!exist_cookie) {
         res.cookie(cookie_name, true, {
-          expires: expires
+          expires: expires,
         });
 
         model.update.view_cnt(body, result => {
-          if(result) {
+          if (result) {
             res.send(true);
           }
         });
-      }    
+      }
     },
 
     password: (req, res) => {
@@ -198,19 +195,19 @@ module.exports = {
 
       model.update.password(body, hash_pw, result => {
         res.send(result);
-      })
+      });
     },
 
     like: (req, res) => {
       const body = req.body;
 
       model.check.like(body, data => {
-        if(data.length === 0) {
+        if (data.length === 0) {
           model.update.like(body, result => {
             res.send(result);
           });
         } else {
-          if(body.type === 'remove') {
+          if (body.type === 'remove') {
             model.update.like(body, result => {
               res.send(result);
             });
@@ -226,8 +223,8 @@ module.exports = {
 
       model.update.board(body, data => {
         res.send(true);
-      })
-    }
+      });
+    },
   },
 
   delete: {
@@ -235,7 +232,7 @@ module.exports = {
       const body = req.body;
 
       model.delete.category(body, result => {
-        if(result) {
+        if (result) {
           res.send(result);
         }
       });
@@ -255,7 +252,7 @@ module.exports = {
       model.delete.reply(body, result => {
         res.send(result);
       });
-    }
+    },
   },
 
   modify: {
@@ -265,7 +262,7 @@ module.exports = {
       model.modify.category(body, result => {
         let obj = {};
 
-        if(result) {
+        if (result) {
           obj['suc'] = true;
           obj['msg'] = 'The category name has been replaced.';
         } else {
@@ -274,13 +271,13 @@ module.exports = {
         }
         res.send(obj);
       });
-    }
+    },
   },
 
   search: {
     id: (req, res) => {
       const body = req.body;
-      console.log('search', body)
+      console.log('search', body);
       model.search.id(body, result => {
         res.send(result);
       });
@@ -292,19 +289,19 @@ module.exports = {
       model.search.pw(body, result => {
         let res_data = {};
 
-        if(result[0]) {
-          const title = 'This is OTP for Aiden\'s Blog';
+        if (result[0]) {
+          const title = "This is OTP for Aiden's Blog";
           const contents = () => {
-            let number = "";
+            let number = '';
             let random = 0;
 
-            for(let i = 0; i < 6; i++) {
-              random = Math.floor(Math.random()*10);
+            for (let i = 0; i < 6; i++) {
+              random = Math.floor(Math.random() * 10);
               number += random;
             }
             res_data['secret'] = number;
-            return `Please type follow number ${number} into authentication box`
-          }
+            return `Please type follow number ${number} into authentication box`;
+          };
 
           const mailOption = mailOpt(result[0].dataValues, title, contents());
 
@@ -312,13 +309,11 @@ module.exports = {
 
           res_data['result'] = result;
           res.send(res_data);
-        
         } else {
           res.send(false);
         }
-        
       });
-    }, 
+    },
   },
 
   check: {
@@ -327,7 +322,7 @@ module.exports = {
 
       model.check.like(body, result => {
         res.send(result);
-      })
-    }
-  }
-} 
+      });
+    },
+  },
+};
